0040bfcc2349a52799becbc787644faf7241524f
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-03-16 10:59:13 +0530

    Switch to PyMySQL

--- config/settings.py
+++ config/settings.py
@@ -13,11 +13,12 @@ from pathlib import Path
 from datetime import timedelta   
 from pathlib import Path
 import os
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+pymysql.install_as_MySQLdb()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
