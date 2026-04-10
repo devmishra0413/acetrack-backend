c3b4a25ba7e5614c0c9dde61d40bb184a483ade0
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-04-10 10:44:16 +0530

    removing pysql 2

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ from pathlib import Path
 from datetime import timedelta   
 from pathlib import Path
 import os
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
