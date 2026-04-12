1f9b3b3f9ab865e7d5a537944c9ab11d271c8a69
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-04-12 13:30:56 +0530

    migration issue

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+from django.core.management import call_command
+import io
+
+def run_migrations(request):
+    out = io.StringIO()
+    call_command('migrate', stdout=out)
+    return HttpResponse(f"<pre>{out.getvalue()}</pre>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +25,7 @@ urlpatterns = [
     path('api/agent/', include('agent.urls')),
     path('api/', include('focus.urls')),
     path('api/', include('leaderboard.urls')),
+
+    #migration
+    path('run-migrations/', run_migrations),
 ]
