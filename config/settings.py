ef6d2f14e9b010ff50546c3e4735d9868e1951a2
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-04-12 13:41:19 +0530

    Add priority field migration

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL', ''),
+        os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
