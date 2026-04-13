3a7edae323aa49b41848ef055313ee20df00055b
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-04-13 16:29:42 +0530

    focus screen issue 5

--- focus/models.py
+++ focus/models.py
@@ -6,7 +6,7 @@ class FocusSession(models.Model):
     user             = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     task             = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=True)
     duration_seconds = models.PositiveIntegerField(default=0)
-    date             = models.DateField()
+    date             = models.DateField(default='2026-01-01')
     created_at       = models.DateTimeField(auto_now_add=True)
 
     def duration_minutes(self):
