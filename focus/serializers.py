91044717cf7c5a7c4f6faae7b4dc188ced49fdfd
Dev Mishra <2k23.csiot2311687@gmail.com>
2026-04-13 12:39:39 +0530

    focus screen issue 3

--- focus/serializers.py
+++ focus/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import FocusSession
 
 class FocusSessionSerializer(serializers.ModelSerializer):
+    task_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+
     class Meta:
         model = FocusSession
-        fields = ['id', 'task', 'duration_seconds', 'date', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['id', 'task', 'task_id', 'duration_seconds', 'date', 'created_at']
+        read_only_fields = ['id', 'created_at', 'task']
